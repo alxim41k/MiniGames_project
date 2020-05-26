bfec48ad3a59c030c5f15d5541e5f47c45860b74
Alh-commit <pokemon2004craft@gmail.com>
2020-05-26 13:59:46 +0500

    Launcher 0.0.1

---
+++ Launcher/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(78, 37);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(78, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nick";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "lvl";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Money:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
--- ServerClient/Serv_Client.cs
+++ ServerClient/Serv_Client.cs
@@ -2,62 +2,64 @@
 using Network;
 using Network.Enums;
 using Network.Converter;
+using System.Text;
+using Network.Extensions;
+
 namespace ServerClient
 {
     public class Server
     {
+        private ServerConnectionContainer serverConnectionContainer;
         public void Main()
         {
-            //1. Create a new server container.
-            
-            ServerConnectionContainer serverConnectionContainer = ConnectionFactory.CreateServerConnectionContainer("127.0.0.1", 1234, false);
-            //2. Apply some settings
+            //1. Start listen on a port
+            serverConnectionContainer = ConnectionFactory.CreateServerConnectionContainer(1234, false);
+
+            //2. Apply optional settings.
+            #region Optional settings
+            serverConnectionContainer.ConnectionLost += (a, b, c) => Console.WriteLine($"{serverConnectionContainer.Count} {b.ToString()} Connection lost {a.IPRemoteEndPoint.Port}. Reason {c.ToString()}");
+            serverConnectionContainer.ConnectionEstablished += connectionEstablished;
             serverConnectionContainer.AllowUDPConnections = true;
-            //3. Set a delegate which will be called if we receive a connection
-            serverConnectionContainer.ConnectionEstablished += ServerConnectionContainer_ConnectionEstablished;
-            //4. Set a delegate which will be called if we lose a connection
-            serverConnectionContainer.ConnectionLost += ServerConnectionContainer_ConnectionLost;
-            //4. Start listening on port 1234
-            serverConnectionContainer.StartTCPListener();
-            
-            Console.ReadLine();
+            serverConnectionContainer.UDPConnectionLimit = 2;
+            #endregion Optional settings
+
+            //Call start here, because we had to enable the bluetooth property at first.
+            serverConnectionContainer.Start();
+        }
+        private void connectionEstablished(Connection connection, ConnectionType type)
+        {
+            Console.WriteLine($"{serverConnectionContainer.Count} {connection.GetType()} connected on port {connection.IPRemoteEndPoint.Port}");
+
+            //3. Register packet listeners.
+            connection.RegisterRawDataHandler("HelloWorld", (rawData, con) => Console.WriteLine($"RawDataPacket received. Data: {rawData.ToUTF8String()}"));
+            connection.RegisterRawDataHandler("BoolValue", (rawData, con) => Console.WriteLine($"RawDataPacket received. Data: {rawData.ToBoolean()}"));
+            connection.RegisterRawDataHandler("DoubleValue", (rawData, con) => Console.WriteLine($"RawDataPacket received. Data: {rawData.ToDouble()}"));
         }
 
-        private void ServerConnectionContainer_ConnectionLost(Connection connection, ConnectionType connectionType, CloseReason closeReason)
-        {
-            Console.WriteLine($"Connection {connection.IPRemoteEndPoint} {connectionType} lost. {closeReason}");
-        }
-
-        private void ServerConnectionContainer_ConnectionEstablished(Connection connection, ConnectionType connectionType)
-        {
-            Console.WriteLine($"{connectionType} Connection received {connection.IPRemoteEndPoint}.");
-        }
     }
     public class Client
     {
-        private ClientConnectionContainer clientConnectionContainer;
         public void Main()
         {
-            
-            //1. Create a new client connection container.
-            clientConnectionContainer = ConnectionFactory.CreateClientConnectionContainer("127.0.0.1", 1234);
-            //2. Setup events which will be fired if we receive a connection
-            clientConnectionContainer.ConnectionEstablished += ClientConnectionContainer_ConnectionEstablished;
-            clientConnectionContainer.ConnectionLost += ClientConnectionContainer_ConnectionLost;
-        }
-        
-        private void ClientConnectionContainer_ConnectionLost(Connection connection, Network.Enums.ConnectionType connectionType, Network.Enums.CloseReason closeReason)
-        {
-            Console.WriteLine($"Connection {connection.IPRemoteEndPoint} {connectionType} lost. {closeReason}");
+            ConnectionResult connectionResult = ConnectionResult.TCPConnectionNotAlive;
+            //1. Establish a connection to the server.
+            TcpConnection tcpConnection = ConnectionFactory.CreateTcpConnection("127.0.0.1", 1234, out connectionResult);
+            //2. Register what happens if we get a connection
+            if (connectionResult == ConnectionResult.Connected)
+            {
+                Console.WriteLine($"{tcpConnection.ToString()} Connection established");
+                //3. Send a raw data packet request.
+                tcpConnection.SendRawData(RawDataConverter.FromUTF8String("HelloWorld", "Hello, this is the RawDataExample!"));
+                tcpConnection.SendRawData(RawDataConverter.FromBoolean("BoolValue", true));
+                tcpConnection.SendRawData(RawDataConverter.FromBoolean("BoolValue", false));
+                tcpConnection.SendRawData(RawDataConverter.FromDouble("DoubleValue", 32.99311325d));
+                //4. Send a raw data packet request without any helper class
+                tcpConnection.SendRawData("HelloWorld", Encoding.UTF8.GetBytes("Hello, this is the RawDataExample!"));
+                tcpConnection.SendRawData("BoolValue", BitConverter.GetBytes(true));
+                tcpConnection.SendRawData("BoolValue", BitConverter.GetBytes(false));
+                tcpConnection.SendRawData("DoubleValue", BitConverter.GetBytes(32.99311325d));
+            }
         }
 
-        private void ClientConnectionContainer_ConnectionEstablished(Connection connection, Network.Enums.ConnectionType connectionType)
-        {
-            Console.WriteLine($"{connectionType} Connection received {connection.IPRemoteEndPoint}.");
-        }
-        private void Send(string Data)
-        {
-            
-        }
     }
 }
